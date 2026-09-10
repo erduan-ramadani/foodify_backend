047cedbecf7187475cab369b7a4c85d42c457c47
Erduan Ramadani <ramadani.erduan@web.de>
2026-09-10 12:31:53 +0200

    chore: initialize Spring Boot project

---
+++ src/main/java/com/eddiapps/foodify/FoodifyBackendApplication.java
@@ -0,0 +1,13 @@
+package com.eddiapps.foodify;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FoodifyBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FoodifyBackendApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/eddiapps/foodify/FoodifyBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.eddiapps.foodify;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FoodifyBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
