b9e60a32e4af1f293cc7b2ec7a298b759a3626a2
Erduan Ramadani <ramadani.erduan@web.de>
2026-09-10 12:32:43 +0200

    feat: add user entity and repository

---
+++ src/main/java/com/eddiapps/foodify/repository/UserRepository.java
@@ -0,0 +1,8 @@
+package com.eddiapps.foodify.repository;
+
+import com.eddiapps.foodify.entity.UserEntity;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface UserRepository extends JpaRepository<UserEntity, Long> {
+}
